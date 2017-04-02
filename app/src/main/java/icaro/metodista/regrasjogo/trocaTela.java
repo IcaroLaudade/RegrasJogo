f47686c2346b3dccb4c364e02fe7e423a33f3a7b
Icaro <icaroalcantara11@hotmail.com>
2017-04-01 22:20:20 -0300

    Regra Jogo- Android

---
+++ app/src/main/java/icaro/metodista/regrasjogo/trocaTela.java
@@ -0,0 +1,16 @@
+package icaro.metodista.regrasjogo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Icaro on 30/03/2017.
+ */
+
+public class trocaTela extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.regras);
+    }
+}
